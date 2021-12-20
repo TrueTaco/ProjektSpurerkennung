147e632267f5e8cff168dfe82d3a3de7301b58a3
Till Becker <tillmbecker@googlemail.com>
2021-12-20 01:31:12 +0100

    Many changes

--- Spurerkennung.py
+++ Spurerkennung.py
@@ -36,89 +36,68 @@ def process_frame(frame):
     left_curve = cv.inRange(img_region_of_interest, lower_white, upper_white)
     right_curve = cv.inRange(img_region_of_interest, (15, 40, 230), (255, 255, 255))
 
+    both_curves = left_curve + right_curve
+
     kernel_small5 = np.array([[0,1,0],[1,1,1],[0,1,0]], 'uint8')
-    left_curve = cv.dilate(left_curve, kernel_small5, iterations=5)
-    right_curve = cv.dilate(right_curve, kernel_small5, iterations=5)
+    both_curves = cv.dilate(both_curves, kernel_small5, iterations=5)
 
-    left_curve_filtered = frame.copy()
-    left_curve_filtered[np.where(left_curve==0)] = 0
-    right_curve_filtered = frame.copy()
-    right_curve_filtered[np.where(right_curve==0)] = 0
-
+    both_curves_filtered = frame.copy()
+    both_curves_filtered[np.where(both_curves==0)] = 0
     
 
     kernel = np.array([[1,1,1],[1,-8,1],[1,1,1]],np.float32)
-    left_curve_filtered = cv.filter2D(left_curve_filtered,-1,kernel)
-    right_curve_filtered = cv.filter2D(right_curve_filtered,-1,kernel)
+    both_curves_filtered = cv.filter2D(both_curves_filtered,-1,kernel)
 
-    return left_curve_filtered,right_curve_filtered
+    return both_curves_filtered
 
-def findFourVertices(img_filtered):
-    height = len(img_filtered)
-    width = len(img_filtered[0])
-
-    startHeight = int(height/1.5)
-    maxWidth = int(width/2)
-    points = []
-
-    tempHeight = startHeight
-    valueFound = False
-    while valueFound == False:
-        # Top left point
-        pointCloud = np.where(img_filtered[startHeight][:maxWidth - 1] != 0)
-        if(len(pointCloud[0]) != 0):
-            # print(pointCloud)
-            xMean = int((pointCloud[0][0] + pointCloud[0][-1])/2)
-            points.append((xMean, startHeight))
-
-            valueFound = True
-        else:
-            startHeight += 1
-
-    startHeight = tempHeight
-    valueFound = False
-    while valueFound == False:
-        # Top right point
-        pointCloud = np.where(img_filtered[startHeight][maxWidth + 1:] != 0)
-        if(len(pointCloud[0]) != 0):
-            xMean = int((pointCloud[0][0] + pointCloud[0][-1])/2)
-            points.append((xMean, startHeight))
-            valueFound = True
-        else:
-            startHeight += 1
-
+def getXValues(img, right):
+    img_hsv = cv.cvtColor(img, cv.COLOR_HSV2BGR)
+    img_gray = cv.cvtColor(img_hsv, cv.COLOR_BGR2GRAY)
     
-    valueFound = False
-    startHeight = height - 1
-    tempHeight = startHeight
-    while valueFound == False:
-        # Bottom left point
-        pointCloud = np.where(img_filtered[startHeight][:maxWidth - 1] != 0)
-        if(len(pointCloud[0]) != 0):
-            # print(pointCloud)
-            xMean = int((pointCloud[0][0] + pointCloud[0][-1])/2)
-            points.append((xMean, startHeight))
-            valueFound = True
+    height = len(img)
+    width = len(img[0])
+
+    maxWidth = int(width/2)
+    leftPoints = []
+    rightPoints = []
+
+    for i in range(height):
+        pointCloudLeft = np.where(img_gray[i][0:maxWidth - 1] != 0)
+        if(len(pointCloudLeft[0]) != 0):
+            xMean = np.mean(pointCloudLeft[0])
+            # xMedian = int((pointCloudLeft[0][0] + pointCloudLeft[0][-1])/2)
+            leftPoints.append(xMean)
+            # print("mean left: ", xMean)
+            # print("median left: ", xMedian)
         else:
-            startHeight -= 1
+            leftPoints.append(0)
 
-    startHeight = tempHeight
-    valueFound = False
-    while valueFound == False:
-        # Bottom right point
-        pointCloud = np.where(img_filtered[startHeight][maxWidth + 1:] != 0)
-        if(len(pointCloud[0]) != 0):
-            xMean = int((pointCloud[0][0] + pointCloud[0][-1])/2)
-            points.append((xMean, startHeight))
-            valueFound = True
+        pointCloudRight = np.where(img_gray[i][maxWidth + 1: width - 1] != 0)
+        if(len(pointCloudRight[0]) != 0):
+            xMean = np.mean(pointCloudLeft[0])
+            # xMedian = int((pointCloudRight[0][0] + pointCloudRight[0][-1])/2)
+            rightPoints.append(xMean)
+            # print("mean right: ", xMean)
+            # print("median right: ", xMedian)
         else:
-            startHeight -= 1
+            rightPoints.append(1)
 
-    print("Points: ", points)
+    return leftPoints, rightPoints
 
+def curveradius(frame, xleft, xright):
+    y = np.linspace(0, frame.shape[0], frame.shape[0])
+    x_m_per_pix = 30.5/720
+    y_m_per_pix = 3.7/720
+    y_eval = np.max(y)
+    
+    curveleft = np.polyfit((y*y_m_per_pix), (np.array(xleft)*x_m_per_pix), 2)
+    curveright = np.polyfit((y*y_m_per_pix), (np.array(xright)*x_m_per_pix), 2)
 
-def perspective_transformation():
-    return "hello"
+    curverad_left = ((1 + (2*curveleft[0]*y_eval*y_m_per_pix + curveleft[1])**2)**1.5) / np.absolute(2*curveleft[0])
+    curverad_right = ((1 + (2*curveright[0]*y_eval*y_m_per_pix + curveright[1])**2)**1.5) / np.absolute(2*curveright[0])
+
+    return curverad_left, curverad_right
+
 
 capture = cv.VideoCapture('img/Udacity/project_video.mp4')
 frameNr = 0
@@ -128,29 +107,58 @@ prevFrameTime = 0
 font = cv.FONT_HERSHEY_SIMPLEX
 
 # success = True
-# capture.set(1,2)
+# capture.set(1,75)
 # success, frame = capture.read()
 
 while(True):
     success, frame = capture.read() 
+    if frameNr == 1 or frameNr == 0:
+        all_curves = process_frame(frame)
 
     if(success):
         newFrameTime = time.time()
 
-        left_curve, right_curve = process_frame(frame)
-        all_curves = left_curve + right_curve
-        findFourVertices(all_curves)
+        # Updates detected lanes every 3 seconds in-order to increase performance
+        # if frameNr%10 == 0:
+        all_curves = process_frame(frame)
+
+        # Perspective transformation:
+        # Chose points:
+        old_points = np.float32([[200,720],[1000,720],[510,460],[690,460]])
+        new_points = np.float32([[0,1200],[720,1200],[0,0],[720,0]])
+
+        # Apply transformation
+        perspective_transform = cv.getPerspectiveTransform(old_points,new_points)
+        all_curves_transformed = cv.warpPerspective(all_curves, perspective_transform,(1200,720))
+        frame_transformed = cv.warpPerspective(frame, perspective_transform,(1200,720))
+
+        # Get current X values of the left line:        
+        leftPoints, rightPoints = getXValues(all_curves_transformed, False)
+        # Calculate 
+        curveRadius = curveradius(all_curves_transformed, leftPoints, rightPoints)
+
         alpha = 0.4
         beta = (1.0 - alpha)
         dark_mask = cv.addWeighted(frame, alpha, all_curves, beta, 0.0)
+
+        birds_eye_view = frame_transformed
         darkened_image = all_curves + dark_mask
+
         fps = 1/(newFrameTime - prevFrameTime)
         prevFrameTime = newFrameTime
         fps = int(fps)
         fps = str(fps)
 
+
         cv.putText(darkened_image, fps, (7, 30), font, 1, (100, 255, 0), 1, cv.LINE_AA)
-        cv.imshow("Current Frame", darkened_image)
+        cv.imshow("Normal POV", darkened_image)
+
+        cv.putText(birds_eye_view, fps, (7, 30), font, 1, (100, 255, 0), 1, cv.LINE_AA)
+        cv.putText(birds_eye_view, str(curveRadius), (7, 50), font, 1, (100, 255, 0), 1, cv.LINE_AA)
+        cv.imshow("Birds-Eye View", birds_eye_view)
+
+
+
 
         frameNr += 1
         key = cv.waitKey(1)
